d4a65761a6d68646e9643b76c2c46dfb9135b5ee
Neil Northrop <nnorthrop@gmail.com>
2014-05-25 09:05:20 -0700

    seed the database with my bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+f = File.open("lib/bookmarks_5_24_14.html", "r")
+
+f.readlines.each do |line|
+  bookmark = Bookmark.new
+  url_match = line.match(/"http.*\/"\s\b|"https.*\/"\s\b|"http.*"/).to_s
+  title_match = line.match(/">.*?.*?</).to_s
+
+  bookmark.title = title_match.gsub("\">", "").gsub("<", "")
+
+  bookmark.url = url_match.split(" ").reverse.pop
+
+  if bookmark.url == "" || bookmark.title == ""
+    puts "EMPTY"
+  else
+    bookmark.save
+  end
+end
+
+f.close
